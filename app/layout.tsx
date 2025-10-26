79d95632ffdb77d7fddf6c3f8c37d66630996287
Sapper <saniamykhailenko@gmail.com>
2025-10-26 22:11:38 +0200

    add metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import 'modern-normalize';
 import './globals.css';
 import Header from '@/components/Header/Header';
 import GeolocationChecker from '@/components/GeolocationChecker/GeolocationChecker';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Currency Converter',
+  description: 'Currency converter',
+};
 
 export default function RootLayout({
   children,
